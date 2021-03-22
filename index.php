6a3a417c99c44db6308efbfb2b9baa1bc1dc7953
Gypsy <71417704+Gypsy-GT@users.noreply.github.com>
2021-03-23 00:21:16 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,136 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="http://localhost/4sem/carrent/index.css">
+    <link rel="stylesheet" href="<link href="
+        https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="main_container">
+        <div class="navbar">
+            <div class="logo">
+                <a href="#">Gypsies Car Rental System</a>
+            </div>
+            <div class="navbar_items">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About us</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contactus">Contact us</a></li>
+                    <li><a href="#ourteam">Our team</a></li>
+                    <li><a href="#">Login/signup</a></li>
+                    <li><a href="#">View cars</a></li>
+
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </ul>
+            </div>
+        </div>
+        <div class="banner_image">
+            <div class="banner_content">
+                <h1>You give us your trust. We will give you our soul</br>
+                    <span>Come and choose a car for your travel</span>
+                </h1>
+                <p>Travel like GYPSIES</p>
+            </div>
+        </div>
+        <div class="about" id="about">
+            <h1 class="title">About us</h1>
+            <p>Gypsies car rental is a full-service provider of ground transportation solutions. We offer a
+                comprehensive fleet & product line guaranteed to fit your transportation needs. Our system is the
+                preferred luxury transportation provider for most of the trevellers in kathmandu valley.
+
+            </p>
+            <div class="btn"><a href="#">Learn more</a></div>
+        </div>
+        <div class="services" id="services">
+            <h1 class="title">Our services</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis saepe officiis suscipit quo porro,
+                optio ipsam non pariatur nam ab commodi sunt, ullam magnam id?</p>
+            <div class="diff_services">
+                <div class="diff_service_item">
+                    <img src="http://localhost/4sem/carrent/collection.png" alt="">
+                    <h3>Wide range of choosing options</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, quibusdam.</p>
+                </div>
+                <div class="diff_service_item">
+                    <img class="sec" src="http://localhost/4sem/carrent/quality.png" alt="">
+                    <h3>Quality cars</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, quibusdam.</p>
+                </div>
+                <div class="diff_service_item">
+                    <img src="http://localhost/4sem/carrent/drivers.png" alt="">
+                    <h3>Experienced drivers</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+        <div class="img-2">
+            <div class="img-txt">
+                <span class="border" style="background-color: black; font-size:25px; color: rgb(103, 207, 18);">Better
+                    car for you</span>
+            </div>
+
+        </div>
+
+
+        <div class="contactus" id="contactus">
+            <h1 class="title">Contact us</h1>
+            <div class="contact_btn"><a href="/opt/lampp/htdocs/4sem/carrent/contact_us.php">Click here</a></div>
+        </div>
+
+
+        <div class="ourteam" id="ourteam">
+            <h1 class="title">Our team</h1>
+            <div class="ourteam_wrapper">
+                <div class="team-1 team">
+                    <div class="team_member">
+                        <img src="/home/gypsy/Pictures/riceee.png" alt="">
+                        <p>Mr:Frodo baggins(CEO)</p>
+                    </div>
+                    <div class="team_member">
+                        <img src="/carrent/badaria.png" alt="">
+                        <p>Mr:Badaria(Marketing manager)</p>
+                    </div>
+                </div>
+                <div class="team-2 team">
+                    <div class="team_member">
+                        <img src="file:///opt/lampp/htdocs/4sem/carrent/girl.png" alt="">
+                        <p>Mr:Success Adhikari(Driver)</p>
+                    </div>
+                    <div class="team_member">
+                        <img src="file:///opt/lampp/htdocs/4sem/carrent/ron.png" alt="">
+                        <p>Ronald Weasly(Driver)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <a href="#">@ 2021 Fourth sem project BCA(By: Jajwlyaman Gautam & Ajay Kumar Yadav)</a>
+        </div>
+        <div>
+            <div class="admin_btn">
+                <a href="http://localhost/4sem/carrent/admin/adminlogin.php"><button>Admin</button></a>
+            </div>
+        </div>
+        <div class="arrow">
+            <a href="#"><img src="/opt/lampp/htdocs/4sem/carrent/cursor.png" alt="up_arrow"></a>
+        </div>
+
+
+    </div>
+    </div>
+
+</body>
+
+</html>
